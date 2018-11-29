62fdcb21e23405e35a32ac5bb9db84753a98d14f
Evgeniy Ryshkov <eugem@macbook-pro-evgeniy.local>
2018-11-29 12:01:50 +0300

    Initial Commit

---
+++ Project1/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Project1
+//
+//  Created by Evgeniy Ryshkov on 29/11/2018.
+//  Copyright © 2018 Evgeniy Ryshkov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Project1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Evgeniy Ryshkov on 29/11/2018.
+//  Copyright © 2018 Evgeniy Ryshkov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
